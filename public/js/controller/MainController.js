93495cab57b88c32eed9e66b23dcc8ead3e6f0ec
Daniel <danielparra93@gmail.com>
2016-07-11 20:20:46 -0400

    angular code

--- public/js/controller/MainController.js
+++ public/js/controller/MainController.js
@@ -3,9 +3,21 @@
   angular.module('ngtodos') //getter
         .controller('MainController', MainController);
 
-  MainController.$inject = [];
+  MainController.$inject = ['$scope', 'TodoService'];
 
-  function MainController(){
-    console.log('Main!');
+  function MainController($scope, TodoService){
+    // console.log('Main!');
+    $scope.message = 'Hey now! What that is sound?';
+    $scope.messages = 'some more stuff';
+    var msg = 'a non scoped messaged';
+    // console.log(TodoService);
+
+
+    var todos;
+    TodoService.readAll()
+                .then(function(){
+                  todos = TodoService.todos;
+                  console.log(todos);
+                });
   }
 })();
---
+++ public/js/services/TodoService.js
@@ -0,0 +1,32 @@
+
+
+(function(){
+  angular.module('ngtodos')
+          .factory('TodoService', TodoService);
+
+  TodoService.$inject = ['$http'];
+
+  function TodoService($http){
+    var o = {
+      create: createTodo, //function
+      readAll: getAll, //function
+      update: updateTodo, //function
+      delete: deleteTodo, //function
+      todos : [] //data
+    };
+    return o;
+
+    function createTodo(){}
+    function getAll(){
+      return $http.get('https://quiet-refuge-27140.herokuapp.com/todos')
+                  .then(function(response){
+                    o.todos = response.data;
+                    // console.log('TodoService todos', todos);
+                  });
+    }
+    function updateTodo(){}
+    function deleteTodo(){}
+
+  }
+
+})()
